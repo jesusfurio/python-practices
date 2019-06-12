5b6e706d7ffcbde554711413cba3233722201153
jesusfurio <44003759+jesusfurio@users.noreply.github.com>
2019-06-12 09:55:16 +0200

    Script to test state of different services.
    Work in progress

--- servicetest.py
+++ servicetest.py
@@ -4,6 +4,7 @@ from enum import Enum
 import requests
 import json
 
+
 def print_head(message):
     print('=' * len(message))
     print(message)
@@ -15,26 +16,23 @@ def print_bottom(message):
     print(message)
     print('=' * len(message))
 
-class Commands:
 
-    network_commands = [['ping','-c 5',input_server_domain(server_ip)],['nmap','-Pn',input_server_domain(server_ip)]]
-    website_commands = [['wget',input_server_domain(server_ip)]]
+def get_network_commands(ip):
+    commands = [['ping', '-c 5', ip], ['nmap', '-Pn', ip]]
 
-    def input_server_domain():
-        server_ip = input("Write the server IP or domain to check:")
-        return server_ip
 
-    def execute_commands():  
+def get_website_commands(ip):
+    commands = [['wget', domain]]
 
-        if function_by_state[option] == 1:
-            for i in commands(network_commands):
-                subprocess.call(i)
-            return subprocess.call(i)
 
-        elif function_by_state[option] == 2:
-            for i in commands(website_commands):
-                subprocess.call(i)
-            return subprocess.call(i)
+def run_commands(option):
+    server_ip = input("Write the server IP or domain to check:")
+
+    if option == 1:
+        commands = get_network_commands(server_ip)
+    elif option == 2:
+        commands = get_website_commands(server_ip)
+    return subprocess.call(commands)
 
 
 class MenuStatus(Enum):
@@ -43,46 +41,49 @@ class MenuStatus(Enum):
     execute_sla = "3-Calculate SLA of Cloud service"
     close_menu = "4-Exit"
 
+
 class Menu:
 
     def date_epoch(date):
-        d = datetime.strptime(date,'%Y-%m-%d %H:%M:%S')
-        convert = d.strftime("%s") 
+        d = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+        convert = d.strftime("%s")
 
         return convert
 
     def input_sla_params():
         try:
-            st_date = input("Start date of the incident. The correct format is YYYY-MM-DD hh:mm:ss : ")
+            st_date = input(
+                "Start date of the incident. The correct format is YYYY-MM-DD hh:mm:ss : ")
             st_epoch = date_epoch(st_date)
 
-            end_date = input("End date of the incident. The correct format is YYYY-MM-DD hh:mm:ss : ")
+            end_date = input(
+                "End date of the incident. The correct format is YYYY-MM-DD hh:mm:ss : ")
             end_epoch = date_epoch(end_date)
 
             ip = input("Write affected IP: ")
 
             return st_epoch, end_epoch, ip
-    
+
         except ValueError:
 
             exit("Invalid date format.")
-    
-    def get_sla(start,end,ip):
-        url = "URL".format(ip)
 
-        response = requests.get(url , params = {"start":start,"end":end})
-    
+    def get_sla(start, end, ip):
+        url = "https://gscan.2i.ovh.net/api/ip/{}/sla".format(ip)
+
+        response = requests.get(url, params={"start": start, "end": end})
+
         if response.status_code == 200:
             payload = response.json()
             availability = payload["availability"]
-            print(availability * 100, "% of time available")    
+            print(availability * 100, "% of time available")
 
         if response.status_code == 500:
             print("Invalid IP address.")
 
     def execute_sla():
         start, end, ip = input_sla_params()
-        get_sla(start,end,ip)
+        get_sla(start, end, ip)
 
     def close_menu():
         print ('Goodbye!')
@@ -96,12 +97,13 @@ class Menu:
 
         return option
 
+
 function_by_state = {
-    0:Menu.main,
-    1:Commands.input_server_domain,
-    2:Commands.input_server_domain,
-    3:Menu.execute_sla,
-    4:Menu.close_menu,
+    0: Menu.main,
+    1: run_commands,
+    2: run_commands,
+    3: Menu.execute_sla,
+    4: Menu.close_menu,
 }
 
 if __name__ == '__main__':
@@ -111,4 +113,4 @@ if __name__ == '__main__':
         if option not in function_by_state:
             print ('Value not correct')
             continue
-        function_by_state[option]()
+        function_by_state[option](option)
